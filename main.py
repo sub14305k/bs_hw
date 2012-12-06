782e9dbe31aca53db5e122fbf1f3af28a98dfce6
sub14305k <blake.swier@mindbodyonline.com>
2012-12-05 17:04:36 -0800

    Updated coursework for unit2 + added google fonts

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import webapp2
 import os
 import jinja2
 import re
+import datetime
 
+now = datetime.datetime.now()
 
 from google.appengine.ext import db
 
@@ -149,6 +151,46 @@ class Welcome(BaseHandler):
         else:
             self.redirect('/unit2/signup')
 
+class Birthday_Form(BaseHandler):
+
+     def get(self):
+        self.render("birthday.html")
+    
+     def post(self):
+
+        calender = {'January' : 31, 'February' : 29, 'March': 31, 'April': 30,'May':31,'June':30,'July':31,'August':31,'September':30,'October':31,'November':30,'December':31}
+
+        month = self.request.get("month")
+        day = self.request.get("day")
+
+        if day:
+            day = int(day)
+
+        year = self.request.get("year")
+        
+        if year:
+            year = int(year)
+
+        submitted_date = 'You submitted: ' + str(month) + ' ' + str(day) + ', ' + str(year)
+
+        if year in range(1900,now.year):
+           
+           days_in_month = calender.get(month)
+           
+           if days_in_month < day:
+                
+                invalid = "Invalid"
+                message = 'Date submitted does not exist, there can only be ' + str(days_in_month) + ' days in ' + str(month)
+                self.render("birthday.html",month = month, day = day, year = year , submitted_date = submitted_date, message = message, invalid = invalid)
+           else:
+                valid = 'Valid'
+                self.render("birthday.html",month = month, day = day, year = year , submitted_date = submitted_date, valid = valid)
+
+        else:
+            
+            error = 'You must enter a year between 1900 and ' + str(now.year)
+            self.render("birthday.html", error = error)
+
 class ASCIIChan(BaseHandler):
 
     def render_ASCII_page(self, title="", art = "", error = ""):
@@ -206,6 +248,7 @@ class Permalink(BaseHandler):
          
 app = webapp2.WSGIApplication([('/', Index),
                                ('/course_work', coursework),
+                               ('/course_work/unit2/birthday', Birthday_Form),
                                ('/course_work/unit3/ASCIIChan', ASCIIChan),
                                ('/unit1/helloudacity', HelloUdacity),
                                ('/unit2/rot13', Rot13),
